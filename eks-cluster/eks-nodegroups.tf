e67e03e46e1a8e39b4c0c0ee8fd43ad70f4a04e1
Mubasher Hassan <81035765+arid1182@users.noreply.github.com>
2026-07-02 22:58:52 +0500

    Update EKS node group scaling configuration

--- eks-cluster/eks-nodegroups.tf
+++ eks-cluster/eks-nodegroups.tf
@@ -15,9 +15,9 @@ resource "aws_eks_node_group" "eks_nodegroups" {
 
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 2
+    desired_size = 3
+    max_size     = 4
+    min_size     = 3
   }
 
   update_config {
